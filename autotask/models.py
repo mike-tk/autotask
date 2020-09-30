eca001384794ec31e4d34d648b8319610c1a1d57
Mike Tkachuk <mike@tkachuk.name>
2020-09-30 10:23:55 +0300

    Fix for newer django
    python_2_unicode_compatible deprecated

--- autotask/models.py
+++ autotask/models.py
@@ -2,7 +2,7 @@
 from datetime import timedelta
 
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
+from six import python_2_unicode_compatible
 from django.utils.timezone import now
 from django.utils.translation import ugettext_lazy as _
 
